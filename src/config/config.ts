0a35f68b8417766593c93a7a695ee269ef02773d
Muhammad Ahsan Zahoor <109317164+codewithahsanzahoor@users.noreply.github.com>
2024-11-06 15:54:13 +0500

    add global error handler middleware

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ configEnv();
 const _config = {
 	port: process.env.PORT,
 	database_url: process.env.MONGO_CONNECTION_STRING,
+	node_env: process.env.ENV,
 };
 
 export const config = Object.freeze(_config);
---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import { config } from "../config/config";
+
+const globalErrorHandler = (
+	err: HttpError,
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const statusCode = err.statusCode || 500;
+
+	res.status(statusCode).json({
+		message: err.message,
+		stack: config.node_env !== "production" ? err.stack : undefined,
+	});
+};
+
+export default globalErrorHandler;
