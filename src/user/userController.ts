f85f19f8af91331364693355391cb054c732605c
Muhammad Ahsan Zahoor <109317164+codewithahsanzahoor@users.noreply.github.com>
2024-11-06 21:50:01 +0500

    setup hash bcrypt password

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import User from "./userModel";
+import bcrypt from "bcrypt";
 
 export const userCreate = async (
 	req: Request,
@@ -17,10 +18,21 @@ export const userCreate = async (
 		return next(error);
 	}
 
+	// check if email already exists
 	const checkEmail = await User.findOne({ email });
 	if (checkEmail) {
 		const error = createHttpError(400, "Email already exists");
 		return next(error);
+	}
+
+	// hash password:
+	const hashedPassword = await bcrypt.hash(password, 10);
+	const user = new User({ name, email, password: hashedPassword });
+	try {
+		await user.save();
+	} catch (error) {
+		const err = createHttpError(500, "Failed to create user: " + error);
+		return next(err);
 	}
 
 	// process
