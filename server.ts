75eb9e03494a04bc96ef2d89c1debda93cf90759
Muhammad Ahsan Zahoor <109317164+codewithahsanzahoor@users.noreply.github.com>
2024-11-06 08:56:02 +0500

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("welcome to the book api");
+import app from "./src/app";
+
+const startServer = () => {
+	const port = process.env.PORT || 3000;
+
+	app.listen(port, () => {
+		console.log("Server is running on port 3000");
+	});
+};
+
+startServer();
