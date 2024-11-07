e1935e881ccf9480f21afa99a74cdca44273d1e6
Muhammad Ahsan Zahoor <109317164+codewithahsanzahoor@users.noreply.github.com>
2024-11-07 23:24:14 +0500

    pnpm setup rather than npm, morgan for route info setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
 	res.json({ message: "Hello World!" });
