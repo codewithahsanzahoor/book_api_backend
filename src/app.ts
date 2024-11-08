3506b49a6af35988a3cb07cbf2008f21c1f081de
Muhammad Ahsan Zahoor <109317164+codewithahsanzahoor@users.noreply.github.com>
2024-11-08 11:12:10 +0500

    book route setup

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,14 @@ import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import morgan from "morgan";
+import bookRouter from "./book/bookRouter";
 
 const app = express();
-
 app.use(express.json());
-
 app.use(morgan("dev"));
 
 app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 
 app.use(globalErrorHandler);
-
 export default app;
---
+++ src/book/bookController.ts
@@ -0,0 +1,5 @@
+import { NextFunction, Request, Response } from "express";
+
+export createBook = async (req: Request, res: Response, next: NextFunction) => {
+    
+};
