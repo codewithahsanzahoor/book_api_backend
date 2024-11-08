d954b18f067a2e3a45552ffb7124c47c26fb8e16
Muhammad Ahsan Zahoor <109317164+codewithahsanzahoor@users.noreply.github.com>
2024-11-08 09:19:58 +0500

    login route create

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { userCreate } from "./userController";
+import { userCreate, userLogin } from "./userController";
 const userRouter = express.Router();
 
 userRouter.post("/register", userCreate);
+
+userRouter.post("/login", userLogin);
 
 export default userRouter;
