04afdedd8559c045c7e536c931ad1fd17b28a4f0
Muhammad Ahsan Zahoor <109317164+codewithahsanzahoor@users.noreply.github.com>
2024-11-09 22:16:52 +0500

    cors origin request setup

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,15 @@ import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import morgan from "morgan";
 import bookRouter from "./book/bookRouter";
+import cors from "cors";
+import { config } from "./config/config";
 
 const app = express();
+app.use(
+	cors({
+		origin: config.frontend_url,
+	})
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
--- src/config/config.ts
+++ src/config/config.ts
@@ -10,6 +10,7 @@ const _config = {
 	cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
 	cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
+	frontend_url: process.env.FRONTEND_URL,
 };
 
 export const config = Object.freeze(_config);
